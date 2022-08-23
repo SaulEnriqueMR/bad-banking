c30502f0ee94f04a6e87b90ffdd0a9e4c3b53633
Saúl Enrique Melchor Ramírez <saulenriquemr@outlook.com>
2022-08-23 00:41:42 -0500

    Succefull transition to create react app

--- banking-app/src/App.js
+++ banking-app/src/App.js
@@ -1,23 +1,33 @@
-// import logo from './logo.svg';
 import './App.css';
-import {HashRouter} from "react-router-dom";
-// import {Navbar} from "react-bootstrap";
+import {HashRouter, Routes, Route} from "react-router-dom";
+import NavBar from "./NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserContext from "./Context";
+import Home from "./pages/Home";
+import CreateAccount from "./pages/CreateAccount";
+import Login from "./pages/Login";
+import Deposit from "./pages/Deposit";
+import Withdraw from "./pages/Withdraw";
+import Balance from "./pages/Balance";
+import AllData from "./pages/AllData";
 
 function App() {
   return (
     <HashRouter>
-      {/*<NavBar/>*/}
-      {/*<UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>*/}
+      <NavBar/>
+      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
         <div className="container" style={{padding: "20px"}}>
-          {/*<Route path="/" exact component={Home} />*/}
-          {/*<Route path="/CreateAccount/" component={CreateAccount} />*/}
-          {/*<Route path="/login/" component={Login} />*/}
-          {/*<Route path="/deposit/" component={Deposit} />*/}
-          {/*<Route path="/withdraw/" component={Withdraw} />*/}
-          {/*<Route path="/balance/" component={Balance} />*/}
-          {/*<Route path="/alldata/" component={AllData} />*/}
+          <Routes>
+            <Route path="/" exact component={Home} />
+            <Route path="/CreateAccount/" component={CreateAccount} />
+            <Route path="/Login/" component={Login} />
+            <Route path="/Deposit/" component={Deposit} />
+            <Route path="/Withdraw/" component={Withdraw} />
+            <Route path="/Balance/" component={Balance} />
+            <Route path="/AllData/" component={AllData} />
+          </Routes>
         </div>
-      {/*</UserContext.Provider>*/}
+      </UserContext.Provider>
     </HashRouter>
   );
 }
---
+++ banking-app/src/Context.js
@@ -0,0 +1,8 @@
+// const Route       = ReactRouterDOM.Route;
+// const Link        = ReactRouterDOM.Link;
+// const HashRouter  = ReactRouterDOM.HashRouter;
+import {createContext} from "react";
+
+const UserContext = createContext(null);
+
+export default UserContext;
