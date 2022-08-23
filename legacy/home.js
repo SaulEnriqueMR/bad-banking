59608dd65ead0093444ab1b7e6fb2bae9fd9d02b
Saúl Enrique Melchor Ramírez <saulenriquemr@outlook.com>
2022-08-22 21:42:13 -0500

    legacy project (not working because can't find cdn files)

---
+++ legacy/home.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      header="BadBank Landing Module"
+      title="Welcome to the bank"
+      text="You can move around using the navigation bar."
+      body={(<img src="./bank.pngng" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
