6a66079a7c37025f0964c36a913e39cb3d4f495b
Saúl Enrique Melchor Ramírez <saulenriquemr@outlook.com>
2022-08-26 02:32:44 -0500

    styles in create account

--- banking-app/src/pages/CreateAccount.js
+++ banking-app/src/pages/CreateAccount.js
@@ -36,27 +36,36 @@ function CreateAccount(){
 	}
 	
 	return (
-			<Card
-					bgcolor="primary"
-					header="Create Account"
-					status={status}
-					body={show ? (
-							<>
-								Name<br/>
-								<input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
-								Email address<br/>
-								<input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
-								Password<br/>
-								<input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-								<button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
-							</>
-					):(
-							<>
-								<h5>Success</h5>
-								<button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
-							</>
-					)}
-			/>
+			<>
+				<div className="home">
+					<div className="container home__layout">
+						<div className="home__content">
+							<Card
+									bgcolor="primary"
+									header="Create Account"
+									status={status}
+									body={show ? (
+											<>
+												Name<br/>
+												<input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+												Email address<br/>
+												<input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+												Password<br/>
+												<input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
+												<button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+											</>
+									):(
+											<>
+												<h5>Success</h5>
+												<button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
+											</>
+									)}
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="credits">By Saúl Enrique Melchor Ramírez</div>
+			</>
 	)
 }
 
